bcc2c0cec8e6110fe672dafc3ebaa9b5d5e0911c
MadhukaXD <99163505+MadhukaXD@users.noreply.github.com>
2022-04-19 15:19:51 +0530

    update

--- src/components/ExerciseList.js
+++ src/components/ExerciseList.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react'
-import styled from "styled-components"
 import axios from "axios"
 import Header from './Header';
 import { NavLink, Link, useLocation } from "react-router-dom";
